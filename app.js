d747abfe537619d629c714e55b7318a0854179f5
lorenamateo90 <lorena.mateo.garcia@gmail.com>
2021-11-08 20:27:11 +0100

    repeat the menu

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ console.clear();
 const main = async() => {
  console.log ('Hola mundo');
 
+ let opt = '';
+ do {
+     opt = await mostrarMenu();
+     console.log({opt});
+
+
+    if ( opt!== '0') await pausa();
+    
+ } while (opt !== '0');
+
+
+
  mostrarMenu();
 
  pausa();
--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -3,42 +3,51 @@ require('colors');
 
 
 const mostrarMenu = () => {
-    console.clear();
-    console.log('====================='.green);
-    console.log('Seleccione una opción'.green);
-    console.log('=====================\n'.green);
 
-    console.log(`${'1'.green} .Crear tarea`);
-    console.log(`${'2'.green} .Listar tareas`);
-    console.log(`${'3'.green} .Listar tareas completadas`);
-    console.log(`${'4'.green} .Listar tareas pedientes`);
-    console.log(`${'5'.green} .Completar tarea(s)`);
-    console.log(`${'6'.green} .Borrar una tarea`);
-    console.log(`${'0'.green} .Salir \n`);
-
-    // Así se crea la interfaz para mostrar y recibir la info del usuario
-    const readline = require ('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    readline.question('Seleccione una opción: ', (opt) => {
-       readline.close();
+    return new Promise (resolve => {
+        console.clear();
+        console.log('====================='.green);
+        console.log('Seleccione una opción'.green);
+        console.log('=====================\n'.green);
     
-    })
+        console.log(`${'1'.green} .Crear tarea`);
+        console.log(`${'2'.green} .Listar tareas`);
+        console.log(`${'3'.green} .Listar tareas completadas`);
+        console.log(`${'4'.green} .Listar tareas pedientes`);
+        console.log(`${'5'.green} .Completar tarea(s)`);
+        console.log(`${'6'.green} .Borrar una tarea`);
+        console.log(`${'0'.green} .Salir \n`);
+    
+        // Así se crea la interfaz para mostrar y recibir la info del usuario
+        const readline = require ('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+    
+        readline.question('Seleccione una opción: ', (opt) => {
+           readline.close();
+           resolve (opt);
+        
+        })
+
+    });
+  
 
 }
 
 // Pausar la aplicación
 const pausa = () => {
-    const readline = require ('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    readline.question(`\n Presione ${'ENTER'.green} para continuar\n `, (opt) => {
-        readline.close();
-     
-     })
+    return new Promise ( resolve => {
+        const readline = require ('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        readline.question(`\n Presione ${'ENTER'.green} para continuar\n `, (opt) => {
+            readline.close();
+            resolve();
+         
+         })
+    })
 }
 
 module.exports = {
